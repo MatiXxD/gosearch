4620f7c99eb243f11c11174e25d52a5ddeca247a
MatiXxD <ilya_dinesenko@mail.ru>
2024-12-25 21:13:44 +0300

    add cache

--- cmd/main.go
+++ cmd/main.go
@@ -2,31 +2,49 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"gosearch/internal/sitescan"
 	"log"
+	"os"
 	"strings"
 )
 
 func main() {
-	wordsFlag := flag.String("s", "", "Enable word search")
+	filename := flag.String("f", "", "Filename")
+	word := flag.String("s", "", "Enable word search")
 	flag.Parse()
 
-	if *wordsFlag == "" {
+	if *word == "" && *filename == "" {
 		log.Fatal("For search use '-s' flag with words to search.")
 	}
 
-	words := strings.Fields(*wordsFlag)
-	sites := []string{"https://go.dev", "https://google.com"}
-
-	foundUrls, err := sitescan.ScanSites(sites, words, 2)
-	if err != nil {
-		log.Fatal(err)
+	var err error
+	var isCached bool
+	var file *os.File
+	if *filename != "" {
+		file, isCached, err = sitescan.PrepareFile(*filename, *word)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
 	}
 
-	for word, urls := range foundUrls {
-		for _, url := range urls {
-			fmt.Printf("%s found in %s\n", word, url)
+	var foundUrls map[string][]string
+	if !isCached || *filename == "" {
+		words := strings.Fields(*word)
+		sites := []string{"https://go.dev", "https://google.com"}
+		foundUrls, err = sitescan.ScanSites(sites, words, 2)
+		if err != nil {
+			log.Fatal(err)
 		}
+	}
+
+	if isCached {
+		_ = sitescan.ReadUrls(file, os.Stdout)
+	} else {
+		_ = sitescan.WriteUrls(foundUrls, os.Stdout)
+	}
+
+	if *filename != "" && !isCached {
+		_ = sitescan.WriteUrls(foundUrls, file)
 	}
 }
